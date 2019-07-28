94ee809739ea10e4a1cca986c247519032aa5a4f
Baktiar Miah <baktiar.m@gmail.com>
2019-07-28 23:48:05 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -109,7 +109,7 @@ def changing_alan
      alans_new_info = "GUI"
       
     puts programmer_hash[:alan_kay][:known_for] = "GUI"
-    return
+    return programmer_hash
 end
 
 def adding_to_dennis
